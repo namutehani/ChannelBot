3eeacc15b3896e48a4ec0e1a877eb3afa1b5cb94
namutehani <58953928+namutehani@users.noreply.github.com>
2022-07-28 22:14:49 +0300

    Update Data.py

--- Data.py
+++ Data.py
@@ -4,59 +4,40 @@ from pyrogram.types import InlineKeyboardButton
 class Data:
     # Start Message
     START = """
-Hey {}
-
-Welcome to {}
-
-You can use me to manage channels with tons of features. Use below buttons to learn more !
-
-By @StarkBots
+Merhaba {}
     """
 
     # Home Button
     home_buttons = [
-        [InlineKeyboardButton(text="🏠 Return Home 🏠", callback_data="home")],
+        [InlineKeyboardButton(text="🏠 Ana Menüye Dön 🏠", callback_data="home")],
     ]
 
     # Rest Buttons
     buttons = [
-        [InlineKeyboardButton("✨ Bot Status and More Bots ✨", url="https://t.me/StarkBots/7")],
         [
-            InlineKeyboardButton("How to Use ❔", callback_data="help"),
-            InlineKeyboardButton("🎪 About 🎪", callback_data="about")
+            InlineKeyboardButton("Nasıl kullanılır ❔", callback_data="help"),
+            InlineKeyboardButton("🎪 Hakkında 🎪", callback_data="about")
         ],
-        [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/StarkBots")],
-        [InlineKeyboardButton("🎨 Support Group 🎨", url="https://t.me/StarkBotsChat")],
     ]
 
     # Help Message
     HELP = """
-Everything is self explanatory after you add a channel.
-To add a channel use keyboard button 'Add Channels' or alternatively for ease, use `/add` command
+Kanal ekle düğmesini kullan veya /add komutunu kullanın!
 
-✨ **Available Commands** ✨
+✨ **Komutlar** ✨
 
-/about - About The Bot
-/help - This Message
-/start - Start the Bot
+/about - Bot hakkında
+/help - Yardım metni
+/start - Botu başlatır.
 
-Alternative Commands
-/channels - List added Channels
-/add - Add a channel
-/report - Report a Problem
+Alternatif komutlar:
+/channels - Eklenimilen kanalları listeler
+/add - Kanal ekler
+/report - Problem bildir
     """
 
     # About Message
     ABOUT = """
-**About This Bot** 
-
-A telegram channel automation bot by @StarkBots
-
-Source Code : [Click Here](https://github.com/StarkBotsIndustries/ChannelBot)
-
-Framework : [Pyrogram](docs.pyrogram.org)
-
-Language : [Python](www.python.org)
-
-Developer : @StarkProgrammer
+**Bot hakkında** 
+    ._.
     """
